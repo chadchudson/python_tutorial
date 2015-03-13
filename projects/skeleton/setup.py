bf945d04ad2bdfcad86a64e77ee16e69c959d2db
chudson0348 <chudson0348@gmail.com>
2015-03-13 08:44:50 -0500

    Adding project Skeleton

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description' : 'My Project',
+    'author' : 'Chad Hudson',
+    'url' : 'www.chad.tennhud.com',
+    'download_url' : 'www.chad.tennhud.com/projects',
+    'author_email' : 'chad@tennhud.com',
+    'version' : '0.1',
+    'install_requires' : ['nose'],
+    'packages' : ['NAME'],
+    'scripts' : [],
+    'name' : 'projectname'
+}
+
+setup(**config)
